3d815acd102d3425c8a8b4a7ac0e653e827f2292
AlexSopa <56882081+AlexSopa@users.noreply.github.com>
2019-10-22 09:56:02 -0500

    Created

---
+++ APCSA/Unit1/AverageSpeedInKM_01_12.java
@@ -0,0 +1,13 @@
+public class AverageSpeedInKM_01_12 {
+
+   public static void main(String[] args) {
+   
+      int miles = 24;
+      double kmpermile = 1.6094;
+      System.out.println(miles * kmpermile);
+      
+   
+   }
+
+
+}
